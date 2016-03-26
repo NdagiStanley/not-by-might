281e2482cbc7e9f4122403c1a7e17c5f51a42e00
Stanley Ndagi <stanley.ndagi@andela.com>
2016-03-26 14:43:47 +0300

    FEATURE Setup Models and their tests, add custom badge in Readme

--- todo/bucketlist/models.py
+++ todo/bucketlist/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Bucketlist(models.Model):
+    name = models.CharField(max_length=20)
+    created_by = models.CharField(max_length=20)
+    date_created = models.DateTimeField(auto_now_add=True, editable=False)
+    date_modified = models.DateTimeField(auto_now=True, editable=False)
+
+    #  Constructor for Bucketlist
+    def __init__(self, name, created_by):
+        self.name = name
+        self.created_by = created_by
+
+    def __str__(self):
+        return '<Bucketlist {}>'.format(self.name)
+
+
+class BucketlistItem(models.Model):
+    title = models.CharField(max_length=30)
+    bucketlist = models.ForeignKey(Bucketlist, related_name="items")
+    date_created = models.DateTimeField(auto_now_add=True, editable=False)
+    date_modified = models.DateTimeField(auto_now=True, editable=False)
+
+    #  Constructor for BucketlistItem
+    def __init__(self, title, bucketlist):
+        self.title = title
+        # self.bucketlist = bucketlist
+
+    def __str__(self):
+        return '<Item {}>'.format(self.title)
--- todo/bucketlist/tests.py
+++ todo/bucketlist/tests.py
@@ -1,5 +1,6 @@
 from django.test import Client, TestCase
 from django.core.urlresolvers import reverse
+from .models import Bucketlist, BucketlistItem
 
 client = Client()
 # Create your tests here.
@@ -7,7 +8,22 @@ client = Client()
 
 class APITests(TestCase):
     """APITests"""
-
     def test_index(self):
         response = client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
+
+
+class BucketlistModelTest(TestCase):
+    """Test Bucketlist Model"""
+    def test_string_representation(self):
+        bucketlist = Bucketlist(name="My entry Bucketlist", created_by="User")
+        self.assertEqual(str(bucketlist),
+                         '<Bucketlist {}>'.format(bucketlist.name))
+
+
+class BucketlistItemModelTest(TestCase):
+    """Test BucketlistItem Model"""
+    def test_string_representation(self):
+        bucketlist = Bucketlist(name="My entry Bucketlist", created_by="User")
+        item = BucketlistItem(title="My entry Bucketlist Item", bucketlist=bucketlist)
+        self.assertEqual(str(item), '<Item {}>'.format(item.title))
