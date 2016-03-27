455151a4274a9fdb25ec340b36f3cbbb0b6afc1f
Stanley Ndagi <stanley.ndagi@andela.com>
2016-03-27 10:41:00 +0300

    FEATURE Task 2 functionality complete

--- todo/todo/api.py
+++ todo/todo/api.py
@@ -6,6 +6,7 @@ from bucketlist.api import BucketlistItemList, BucketlistItemDetail
 
 
 urlpatterns = [
+    url(r'auth/login/', 'rest_framework_jwt.views.obtain_jwt_token'),
     url(r'^bucketlists/$', BucketlistList.as_view()),
     url(r'^bucketlists/(?P<pk>[0-9]+)$', BucketlistDetail.as_view()),
     url(r'^bucketlists/(?P<bucketlist_id>[0-9]+)/items/$', BucketlistItemList.as_view()),
