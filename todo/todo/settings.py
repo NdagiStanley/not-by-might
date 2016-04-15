24150fd413a959a0ca5df389113e7475cf6d3942
Stanley Ndagi <stanley.ndagi@andela.com>
2016-04-15 11:47:29 +0300

    [Feature] Fix heroku build

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -92,6 +92,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
