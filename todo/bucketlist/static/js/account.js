26e6c1d8f45e6e43e151f7ac46ff5eccc7b4cc2a
Stanley Ndagi <stanley.ndagi@andela.com>
2016-04-24 23:27:42 +0300

    [Feature] Login and Register API calls from frontend working

--- todo/bucketlist/static/js/account.js
+++ todo/bucketlist/static/js/account.js
@@ -1,168 +1,104 @@
 Vue.config.delimiters = ["[[", "]]"]
 
-
 // For Register. This is our root page
 var registerComponent = Vue.extend({
-    template: '<h2 class="section-heading">Register</h2>' +
-      '<div id="register">' +
-        '<div class="alert alert-danger" v-if="status.error">Some error message</div>' +
-        '<form name="signup" id="Account">' +
-        '<div class="row">' +
-          '<div class="form-group">' +
-            '<input type="text" class="form-control" placeholder="Your username *" id="name" required data-validation-required-message="Please enter your name." v-model="user.username">' +
-            '<p class="help-block text-danger"></p>' +
-          '</div>' +
-          '<div class="form-group">' +
-            '<input type="email" class="form-control" placeholder="Your Email *" id="email" required data-validation-required-message="Please enter your email address." v-model="user.email">' +
-            '<p class="help-block text-danger"></p>' +
-          '</div>' +
-          '<div class="form-group">' +
-            '<input type="password" class="form-control" placeholder="Password *" id="password" required data-validation-required-message="Please enter the password you are to use." v-model="user.password">' +
-            '<p class="help-block text-danger"></p>' +
-          '</div>' +
-          '<div class="form-group">' +
-            '<input type="password" class="form-control" placeholder="Confirm Password *" id="password" required data-validation-required-message="Please confirm the password you entered." v-model="user.conf_password">' +
-            '<p class="help-block text-danger"></p>' +
-          '</div>' +
-          '<div>' +
-            '<div id="success"></div>' +
-            '<button type="submit" class="btn btn-xl" v-on="click: signUp">Sign Up</button>' +
-          '</div>' +
-        '</div>' +
-        '</form>' +
-      '</div>' +
-      '<br >' +
-      '<p>Already, having an account?</p>' +
-      '<h4> <a v-link="{ path: \'/login\' }">Login</a></h4>'
-})
-
-new Vue({
-
-    // We want to target the div with an id of 'Register'
-    el: '#register',
-
-    data: {
-        user: {username: '', password: '', conf_password: '', email: '' },
-        users: [],
-        status: {
-          success: false,
-          error: false
-        }
-    },
-
-    // Anything within the ready function will run when the application loads
-    ready: function() {},
-
-    // Methods we want to use in our application are registered here
+    template: '#register_form',
     methods: {
         signUp: function() {
-            console.log('Hello');
-            this.$http.post('/api/vi/auth/register', user).then(function(response) {
-              console.log(this.status)
-              this.status.success = true;
-              console.log("Yei");
-              // Redirect
-            }, function() {
-              this.status.error = true;
-            })
-            // this.$http.post('/api/v1/auth/register/', user).success(function() {
-            //   // show success
-            //   html = "<div class='alert alert-success' role='alert'>Sign up successful.</div>";
-            //   $("#signUpMessage").html(html);
-            // })
-
-            console.log("Anyone?");
-            if (this.user.username) {
-                console.log("Get in here");
-                this.users.push(this.user);
-                this.user = { username: '', email: '', password: '', conf_password: '' };
-            }
+            console.log(this.$data.user);
+            this.$http.post('/api/v1/auth/register/', this.$data.user).then(function(response) {
+                            this.$set('status',
+                                      response.status == '201' ? 'Signing you up' : 'ERROR');
+                            console.log(response.status);
+                        })
         }
     }
-});
+})
 
-
-// Login page content
 var loginComponent = Vue.extend({
-    template: '<h2 class="section-heading">Login</h2>' +
-      '<div id="login">' +
-        '<form name="signup" id="Account">' +
-        '<div class="row">' +
-          '<div class="form-group">' +
-            '<input type="text" class="form-control" placeholder="Your username *" id="name" required data-validation-required-message="Please enter your username." v-model="user.username">' +
-            '<p class="help-block text-danger"></p>' +
-          '</div>' +
-          '<div class="form-group">' +
-            '<input type="password" class="form-control" placeholder="Password *" id="password" required data-validation-required-message="Please enter your password." v-model="user.password">' +
-            '<p class="help-block text-danger"></p>' +
-          '</div>' +
-          '<div>' +
-            '<div id="success"></div>' +
-            '<button type="submit" class="btn btn-xl" v-on="click: signIn">Login</button>' +
-          '</div>' +
-        '</div>' +
-        '</form>' +
-      '</div>' +
-      '<br >' +
-      '<p>You don\'t have an account?</p>' +
-      '<h4><a v-link="{ path: \'/\' }">Register</a> </h4>'
-})
+            template: '#login_form',
+            methods: {
+                signIn: function() {
+                    console.log(this.$data.user)
+                    this.$http.post('/api/v1/auth/login/', this.$data.user).then(function(response) {
+                            // get status
+                            response.status;
 
-new Vue({
+                            // get all headers
+                            response.headers();
 
-    // We want to target the div with an id of 'Login'
-    el: '#login',
+                            // get 'expires' header
+                            response.headers('expires');
 
-    data: {
-        user: { username: '', password: '' },
-        users: []
-    },
+                            // set data on vm
+                            this.$set('status',
+                                      response.status == '200' ? 'Loginning you in' : 'ERROR');
+                            console.log(response.data.token);
+                            // localStorage.setItem('token': response.data.token);
+                        })
+                    }
+                }
+            })
 
-    ready: function() {},
+        new Vue({
 
-    methods: {
-        signIn: function() {
-            // this.$http.post('/api/v1/auth/login/').success(function() {
-            //   // show success
-            //   html = "<div class='alert alert-success' role='alert'>Logging in you in...</div>";
-            //   $("#signInMessage").html(html);
-            if (this.user.username) {
-                this.users.push(this.user);
-                console.log(this.user)
-                this.user = { username: '', password: '' };
+            // We want to target the div with an id of 'Register'
+            el: '#app',
+
+            components: {
+                registerComponent,
+                loginComponent
+            },
+
+            data: {
+                user: { username: 'abc', password: 'abc', confirm_password: '', email: '' },
+                users: []
+            },
+
+            ready: function() {},
+
+            methods: {
+                validate: function() {
+                    alert('Signing in');
+                },
+                signUp: function() {
+                    console.log(user)
+                    alert('Signing in');
+                },
+
+                signIn: function(user) {
+                    console.log(user)
+                }
             }
-        }
-    }
-});
 
+        });
 
-// Tell Vue to use view-router
-Vue.use(VueRouter)
+        // Tell Vue to use view-router
+        Vue.use(VueRouter)
 
-// Router options
-var router = new VueRouter({
-    history: false,
-    root: '/'
-})
+        // Router options
+        var router = new VueRouter({
+            history: false,
+            root: '/'
+        })
 
-// Router map for defining components
-router.map({
+        // Router map for defining components
+        router.map({
 
-    '/': {
-        component: registerComponent
-    },
+            '/': {
+                component: registerComponent
+            },
 
-    '/login': {
-        component: loginComponent
-    }
-});
+            '/login': {
+                component: loginComponent
+            }
+        });
 
-// Redirect to the home route if any routes are unmatched
-router.redirect({
-  '*': '/'
-})
+        // Redirect to the home route if any routes are unmatched
+        router.redirect({
+            '*': '/'
+        })
 
+        var App = Vue.extend({})
 
-var App = Vue.extend()
-
-router.start(App, '#account')
+        router.start(App, '#app')
