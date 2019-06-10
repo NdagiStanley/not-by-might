5216c066c0869056e47259b33e42f375366f022c
Ndagi Stanley <stanley.ndagi@andela.com>
2019-06-11 00:17:46 +0300

    Fix test

--- todo/bucketlist/migrations/0001_initial.py
+++ todo/bucketlist/migrations/0001_initial.py
@@ -15,22 +15,26 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Bucketlist',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID',
+                                        serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=20)),
                 ('date_created', models.DateTimeField(auto_now_add=True)),
                 ('date_modified', models.DateTimeField(auto_now=True)),
-                ('created_by', models.ForeignKey(related_name='bucketlists', to=settings.AUTH_USER_MODEL)),
+                ('created_by', models.ForeignKey(related_name='bucketlists',
+                                                 to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)),
             ],
         ),
         migrations.CreateModel(
             name='BucketlistItem',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID',
+                                        serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=30)),
                 ('done', models.BooleanField(default=False)),
                 ('date_created', models.DateTimeField(auto_now_add=True)),
                 ('date_modified', models.DateTimeField(auto_now=True)),
-                ('bucketlist', models.ForeignKey(related_name='items', to='bucketlist.Bucketlist')),
+                ('bucketlist', models.ForeignKey(
+                    related_name='items', to='bucketlist.Bucketlist')),
             ],
         ),
     ]
